3537dd0aaf077357892389a1e2b68ab8b19b150f
pedro <pedrofreitashv@gmail.com>
2026-08-31 16:11:00 -0300

    remove env do repositorio

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom"
 
-export function PrivateRoute({ children }: { children: JSX.Element }) {
+export function PrivateRoute({ children }: { children: React.ReactNode }) {
   const user = localStorage.getItem("user")
 
   if (!user) {
